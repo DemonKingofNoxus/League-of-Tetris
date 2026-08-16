5d654841762281f693276c8a8a6fbe440e4eea86
budala187 <kojatobudalapucanamene@gmail.com>
2026-08-16 19:06:28 +0200

    fixed link

--- src/supabase-config.js
+++ src/supabase-config.js
@@ -15,7 +15,7 @@ window.LOL = window.LOL || {};
 
 LOL.SUPABASE = {
   /* Project URL, e.g. 'https://abcdefghijklm.supabase.co' */
-  url: 'https://league-of-tetris-one.vercel.app/',
+  url: 'https://cjbhgrvgvntuolslizbp.supabase.co/',
 
   /* Project API key marked "anon" / "public" */
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqYmhncnZndm50dW9sc2xpemJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4OTI0MjAsImV4cCI6MjEwMjQ2ODQyMH0.EEBcGNFs83Q3nxWAVbuPZXV7HCwElJNokLr0ljtzB_k',
